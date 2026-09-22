694a6c8c9aeb946006d0acaae2136ddcb6e4b079
Halleyscomet08 <plpayte327@gmail.com>
2026-09-22 09:33:51 +0800

    feat: added findbyBrandId query for Product

--- src/main/java/com/inventoryProject/repositories/ProductRepository.java
+++ src/main/java/com/inventoryProject/repositories/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.inventoryProject.repositories;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.inventoryProject.models.Product;
@@ -8,5 +9,7 @@ import com.inventoryProject.models.Product;
  * StockRepository
  */
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+  public List<Product> findByBrand_BrandId(Long brandId);
 
 }
