ed5dfdb2c3708fe2af3fb436bb2c023e2dae4caf
Halleyscomet08 <plpayte327@gmail.com>
2026-09-24 10:38:38 +0800

    feat: added 400 exception for malformed request format

--- src/main/java/com/inventoryProject/exception/GlobalExceptionHandler.java
+++ src/main/java/com/inventoryProject/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -27,6 +28,11 @@ public class GlobalExceptionHandler {
     ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
 
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+  }
+
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ResponseEntity<String> handleTypeErrors(HttpMessageNotReadableException ex) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid Request Format");
   }
 
   @ExceptionHandler(Exception.class)
