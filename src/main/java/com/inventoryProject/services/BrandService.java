9dcee2601b4007dae867e25997e43178935d63f6
Halleyscomet08 <plpayte327@gmail.com>
2026-09-24 21:15:11 +0800

    feat: added archive function to Brand Service

--- src/main/java/com/inventoryProject/services/BrandService.java
+++ src/main/java/com/inventoryProject/services/BrandService.java
@@ -3,8 +3,12 @@ package com.inventoryProject.services;
 import org.springframework.stereotype.Service;
 
 import com.inventoryProject.dto.BrandDTO;
+import com.inventoryProject.exception.ResourceNotFoundException;
 import com.inventoryProject.models.Brand;
+import com.inventoryProject.models.EntityMode;
 import com.inventoryProject.repositories.BrandRepository;
+
+import jakarta.transaction.Transactional;
 
 /**
  * BrandService
@@ -20,7 +24,7 @@ public class BrandService {
 
   public BrandDTO get(Long brandId) {
     return toDTO(brandRepository.findById(brandId)
-        .orElseThrow(() -> new RuntimeException("Brand not found")));
+        .orElseThrow(() -> new ResourceNotFoundException("Brand not found")));
   }
 
   public BrandDTO create(BrandDTO dto) {
@@ -31,6 +35,19 @@ public class BrandService {
 
     return toDTO(saved);
 
+  }
+
+  @Transactional(rollbackOn = Exception.class)
+  public void archiveBrand(Long brandId) {
+    Brand brand = brandRepository.findById(brandId)
+        .orElseThrow(() -> new ResourceNotFoundException("Product Not Found"));
+    // in the future once the variants table is setup, query for variants
+    // where productID = product.getproductId(), foreach(variant ->
+    // variant.setdeletedAt(current_date))
+    // For now,
+    brand.setStatus(EntityMode.ARCHIVED);
+
+    brandRepository.save(brand);
   }
 
   private BrandDTO toDTO(Brand brand) {
