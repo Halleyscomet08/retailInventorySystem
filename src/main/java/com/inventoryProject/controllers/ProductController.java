64acb455399cb96817041cdfadaac461c67b65e9
Halleyscomet08 <plpayte327@gmail.com>
2026-09-24 09:53:20 +0800

    feat: created update api for product

--- src/main/java/com/inventoryProject/controllers/ProductController.java
+++ src/main/java/com/inventoryProject/controllers/ProductController.java
@@ -2,20 +2,20 @@ package com.inventoryProject.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inventoryProject.services.ProductService;
 import com.inventoryProject.dto.ProductResponseDTO;
 import com.inventoryProject.dto.Productdto;
-import com.inventoryProject.models.Product;
 
 /**
  * ProductController
@@ -31,15 +31,9 @@ public class ProductController {
     this.productService = productService;
   }
 
-  @GetMapping("/all")
-  @ResponseBody
+  @GetMapping
   public ResponseEntity<List<ProductResponseDTO>> getAll() {
     return ResponseEntity.ok(productService.findAll());
-  }
-
-  @PostMapping("/create")
-  public ResponseEntity<ProductResponseDTO> create(@RequestBody Productdto dto) {
-    return ResponseEntity.ok(productService.create(dto));
   }
 
   @GetMapping("/{productId}")
@@ -48,8 +42,18 @@ public class ProductController {
   }
 
   // function that manages output of products
-  @GetMapping("brand/{brandId}")
+  @GetMapping("/brand/{brandId}")
   public ResponseEntity<List<ProductResponseDTO>> getByBrand(@PathVariable Long brandId) {
     return ResponseEntity.ok(productService.getByBrand(brandId));
+  }
+
+  @PostMapping
+  public ResponseEntity<ProductResponseDTO> create(@RequestBody Productdto dto) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(productService.create(dto));
+  }
+
+  @PutMapping("/{productId}")
+  public ResponseEntity<ProductResponseDTO> update(@PathVariable Long productId, @RequestBody Productdto dto) {
+    return ResponseEntity.ok(productService.update(productId, dto));
   }
 }
