558756989d3d3c8c14c8b4499780bf762c51a210
Halleyscomet08 <plpayte327@gmail.com>
2026-09-24 21:14:40 +0800

    feat: added status filter for brand repository

--- src/main/java/com/inventoryProject/repositories/BrandRepository.java
+++ src/main/java/com/inventoryProject/repositories/BrandRepository.java
@@ -1,12 +1,18 @@
 package com.inventoryProject.repositories;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.inventoryProject.models.Brand;
+import com.inventoryProject.models.EntityMode;
 
 /**
  * BrandRepository
  */
 public interface BrandRepository extends JpaRepository<Brand, Long> {
 
+  @Query("SELECT b FROM Brand b WHERE b.status = :status")
+  public List<Brand> findAllByStatus(@Param("status") EntityMode status);
 }
